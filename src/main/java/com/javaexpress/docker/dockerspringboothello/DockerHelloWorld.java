d5ff4aed37eda9723149ffe2a49cdfbf56b97aca
ram4599 <125234857+ram4599@users.noreply.github.com>
2023-02-13 10:08:18 +0530

    commit

--- src/main/java/com/javaexpress/docker/dockerspringboothello/DockerHelloWorld.java
+++ src/main/java/com/javaexpress/docker/dockerspringboothello/DockerHelloWorld.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("")
+@RequestMapping("/v1/docker")
 public class DockerHelloWorld {
 
 	@GetMapping
